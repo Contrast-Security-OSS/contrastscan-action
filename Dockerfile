d7b8309cbc2f83382744e929d2385be30b443785
Mark Weeks <marrkkuu@gmail.com>
2022-05-19 15:34:06 +0100

    WIP

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 # Container image that runs your code
-FROM openjdk:jdk
+FROM node:alpine
+
+RUN npm install -g @contrast/contrast-cli && npm install -g node-jq
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
-COPY contrastscan-remote/target/contrastscan-remote-*.jar /contrastscan-remote.jar
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,25 @@
 #!/bin/sh -l
 
-echo "scanning artifact $INPUT_ARTIFACT"
-echo "output_sarif $INPUT_SARIF"
-echo "url $INPUT_API_URL"
-java -jar /contrastscan-remote.jar --sarif-out=$INPUT_SARIF --url=$INPUT_API_URL $INPUT_ARTIFACT
+echo "Org ID: $INPUT_ORGID"
+echo "Project Name: $INPUT_PROJECTNAME"
+echo "API URL $INPUT_APIURL"
+echo "Artifact: $INPUT_ARTIFACT"
+echo "SARIF Output: $INPUT_SARIF"
+echo "Languages: $INPUT_LANGUAGE"
+echo "Timeout: $INPUT_TIMEOUT"
+echo "WaitForScan: $INPUT_WAITFORSCAN"
+echo "SaveScanResults: $INPUT_SAVESCANRESULTS"
+
+set -x #echo on
+
+contrast-cli --scan "$INPUT_ARTIFACT" --api_key "$INPUT_APIKEY" \
+ --authorization "$INPUT_AUTHHEADER" --organization_id "$INPUT_ORGID" --host "$INPUT_APIURL" \
+ --project_name "$INPUT_PROJECTNAME" ${INPUT_LANGUAGE:+"--language"} ${INPUT_LANGUAGE:+"$INPUT_LANGUAGE"} --scan_timeout "${INPUT_TIMEOUT:-300}" \
+ ${INPUT_WAITFORSCAN:+"--wait_for_scan"} ${INPUT_SAVESCANRESULTS:+"--save_scan_results"} ${INPUT_SAVESCANRESULTS:+"--results_file_name"} ${INPUT_SAVESCANRESULTS:+"$INPUT_SARIF"}
+
+export CONTRAST_CLI_EXIT_CODE=$?
+
+if [ ${CONTRAST_CLI_EXIT_CODE} != 0 ]; then
+  echo "Contrast Scan failed. Please contact support"
+  exit 1
+fi
