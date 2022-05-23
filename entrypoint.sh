d089960cb2d662d5fa9e4ce5593ebbedb7ba9337
Mark Weeks <marrkkuu@gmail.com>
2022-05-23 16:18:44 +0100

    tidy up

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,11 @@ echo "Project Name: $INPUT_PROJECTNAME"
 echo "Project ID: $INPUT_PROJECTID"
 echo "API URL $INPUT_APIURL"
 echo "Artifact: $INPUT_ARTIFACT"
-echo "SARIF Output: $INPUT_SARIF"
-echo "Languages: $INPUT_LANGUAGE"
+echo "Language: $INPUT_LANGUAGE"
 echo "Timeout: $INPUT_TIMEOUT"
-echo "SaveScanResults: $INPUT_SAVESCANRESULTS"
-
-set -x #echo on
 
 /usr/bin/contrast scan --file "$INPUT_ARTIFACT" --api-key "$INPUT_APIKEY" --authorization "$INPUT_AUTHHEADER" \
- ${INPUT_ORGID:+"--organization-id"} ${INPUT_ORGID:+"$INPUT_ORGID"} --host "$INPUT_APIURL" \
+ --organization-id "$INPUT_ORGID" --host "$INPUT_APIURL" \
  ${INPUT_PROJECTNAME:+"--name"} ${INPUT_PROJECTNAME:+"$INPUT_PROJECTNAME"} \
  ${INPUT_PROJECTID:+"--project-id"} ${INPUT_PROJECTID:+"$INPUT_INPUT_PROJECTID"}  \
  ${INPUT_LANGUAGE:+"--language"} ${INPUT_LANGUAGE:+"$INPUT_LANGUAGE"} --timeout "${INPUT_TIMEOUT}" \
