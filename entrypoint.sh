446a57b6f686fdaf711d523fc1d2542f48c8acc9
Noah Irwin <noah.irwin@contrastsecurity.com>
2022-09-12 14:46:35 +0100

    SCAN-4662 Add fail flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,20 @@ echo "Timeout: $INPUT_TIMEOUT"
 [ -z "$INPUT_APIKEY" ] && echo "Contrast API Key is required but not present" && exit 1;
 [ -z "$INPUT_AUTHHEADER" ] && echo "Contrast Authorization Header is required but not present" && exit 1;
 
+if [ -n "$INPUT_SEVERITY" ]
+then
+  if [ -z "$INPUT_FAIL" ] || [ "$INPUT_FAIL" != true ]
+  then
+    echo "ERROR: Severity has been set without fail flag. Please set fail to true in order to fail the action if vulnerabilities are found."
+    exit 1
+  fi
+fi
+
+if [ "$$INPUT_FAIL" = true ]
+then
+  FAIL=true
+fi
+
 export CONTRAST_CODESEC_CI=true
 export CODESEC_INVOCATION_ENVIRONMENT="GITHUB"
 
@@ -21,6 +35,7 @@ export CODESEC_INVOCATION_ENVIRONMENT="GITHUB"
  ${INPUT_PROJECTNAME:+"--name"} ${INPUT_PROJECTNAME:+"$INPUT_PROJECTNAME"} \
  ${INPUT_PROJECTID:+"--project-id"} ${INPUT_PROJECTID:+"$INPUT_PROJECTID"}  \
  ${INPUT_LANGUAGE:+"--language"} ${INPUT_LANGUAGE:+"$INPUT_LANGUAGE"}  \
+ ${FAIL:+"--fail"} ${INPUT_SEVERITY:+"--severity"} ${INPUT_SEVERITY:+"$INPUT_SEVERITY"}  \
  --timeout "${INPUT_TIMEOUT}" -s sarif
 
  CONTRAST_RET_VAL=$?
